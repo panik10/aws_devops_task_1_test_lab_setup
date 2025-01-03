7fb30fd0a6507f4ed27d1774ce8b647cc4871e79
Bohdan Mateiko <bohdan.id27182@outlook.com>
2025-01-03 22:52:50 +0200

    adds tags for the VPC resource

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 # Uncomment the resource bellow to complete the task: 
 # resource "aws_vpc" "network" {
 #   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "mate-aws-grafana-lab"
+#   }
 # }
